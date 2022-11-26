272edcab69641390d4f0f90c34e3303381c0ce0f
Valek <pasport96@gmail.com>
2022-11-26 16:36:07 +0300

    README

---
+++ final test/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] arr1 = {"Hello", "2", "world", ":-)"};
+string[] arr2 = new string[arr1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(arr1, arr2);
+PrintArray(arr2);
